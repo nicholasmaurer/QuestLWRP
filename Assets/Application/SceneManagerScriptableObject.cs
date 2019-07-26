07208cd6b83583b66ef409abcd7d5dccc94a8362
Nicholas Maurer <nicholasallenmaurer@gmail.com>
2019-07-26 14:23:21 +1000

    updated plugin and got vfx working with points

---
+++ Assets/Application/SceneManagerScriptableObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SceneManagerScriptableObject", order = 1)]
+public class SceneManagerScriptableObject : ScriptableObject
+{
+    public Scene[] Scenes;
+}
--- Assets/ApplicationController.cs
+++ Assets/ApplicationController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ApplicationController : MonoBehaviour
 {
+    public int currentScene;
+
+    public int sceneCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 90;
     }
 
     // Update is called once per frame
