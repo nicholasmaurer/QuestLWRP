8de7637364c8f20a49ad51124209798d93ad12a2
Nicholas Maurer <nicholasallenmaurer@gmail.com>
2019-08-28 00:45:05 +1000

    Alices Parachute scene

---
+++ Assets/ParachuteForce.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParachuteForce : MonoBehaviour
+{
+    public ConstantForce constantForce;
+    [Header("xzMin, xzMax, yMin, yMax")]
+    public Vector4 force;
+
+    public Vector4 relativeForce;
+
+    public Vector4 torque;
+
+    public Vector3 relativeTorque;
+
+    private Vector3 randomForce;
+
+    private Vector3 randomRelativeForce;
+
+    private Vector3 randomTorque;
+
+    private Vector3 randomRelativeTorque;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Random.Range(force.x, force.y);
+        float z = Random.Range(force.x, force.y);
+        float y = Random.Range(force.z, force.w);
+        randomForce.x = x;
+        randomForce.y = y;
+        randomForce.z = z;
+        constantForce.force = randomForce;
+    }
+}
---
+++ Assets/TimedTransition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedTransition : MonoBehaviour
+{
+    public float seconds = 60;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneController.Instance.StartCoroutine(SceneController.Instance.LoadNextScene());
+    }
+}
